ea95504f7e98d14a736c73b434e5135687cfffea
latracheaziz <azizlatrache5@gmail.com>
2025-12-06 18:49:04 +0100

    modifaction dans le code

--- backend/server.js
+++ backend/server.js
@@ -13,8 +13,8 @@ app.get('/api/test', (req, res) => {
 
 // Données mock
 const logements = [
-  { id: 1, titre: "Appartement centre-ville", prix: 50000, ville: "Tunis" },
-  { id: 2, titre: "Maison avec jardin", prix: 120000, ville: "Sousse" },
+  { id: 1, titre: "Appartement centre-ville", prix: 500000, ville: "Tunis" },
+  { id: 2, titre: "Maison avec jardin", prix: 1200000, ville: "Sousse" },
   { id: 3, titre: "Studio proche université", prix: 25000, ville: "Monastir" },
 ];
 
